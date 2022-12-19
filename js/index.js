48ca3a2d478969085e0c8d5876457b8595c01909
Gabriel Lacerda <gabriellacerdacosta07@gmail.com>
2022-12-19 16:50:46 -0300

    Fazendo validação de user com um único usuário

--- js/index.js
+++ js/index.js
@@ -3,26 +3,54 @@ btnSubmit.addEventListener('click', (event) => {
   event.preventDefault()
   const userInput = document.querySelector('#user')
   const passwordInput = document.querySelector('#password')
-  verificaUser(userInput, passwordInput)
+  checkInputs(userInput, passwordInput)
+  // verificaUser(userInput, passwordInput)
 })
 
-async function verificaUser(userInput, passwordInput) {
-  const usersBd = await getUsersBd()
-  const arrUsersBd = usersBd.users
-  arrUsersBd.forEach(userBd => {
-    if (userInput.value == userBd.User.email && passwordInput.value == userBd.User.senha) {
-      window.location.href = 'home.html'
-    } else {
-      erroLogin()
-    }
-  })
+function checkInputs(userInput, passwordInput) {
+  if (userInput.value === "") {
+    setErrorFor(userInput, 'O usuário é obrigatório!');
+  } else if (!checkEmail(userInput.value)) {
+    setErrorFor(userInput, 'Insira um email válido');
+  } else if (validateUserDb(userInput)) {
+    setErrorFor(userInput, 'Usuário não cadastrado');
+  } else {
+    setSuccessFor(userInput)
+  }
+  // if (passwordInput.value === "") {
+  //   setErrorFor(passwordInput, 'A senha é obrigatória!');
+  // } else {
+  //   setSuccessFor(passwordInput)
+  // }
 }
-function erroLogin(userInput, passwordInput) {
-  if (!userInput) {
-
+function setErrorFor(input, message) {
+  const divForm = input.parentElement
+  const small = divForm.querySelector("small")
+  input.className = 'border-error'
+  small.innerText = message
+  small.className = 'error'
+}
+function setSuccessFor(input) {
+  const divForm = input.parentElement
+  const small = divForm.querySelector("small")
+  input.className = 'border-success'
+  small.className = 'success'
+  small.innerText = ''
+}
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
+//
+async function validateUserDb(userInput) {
+  const usersDb = await getUsersDb()
+  const userDb = usersDb.users[0].User.email
+  if (userInput.value === userDb) {
+    setSuccessFor(userInput)
   }
 }
-async function getUsersBd() {
+async function getUsersDb() {
   const response = await fetch(`http://localhost/seuJoaoApi/users.json`)
   const data = await response.json()
   return data
