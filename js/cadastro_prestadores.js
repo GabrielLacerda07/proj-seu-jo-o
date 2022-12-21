58c6a1fa01ba600d27dcfbdd8ec8fb8aae2cf19a
Gabriel Lacerda <gabriellacerdacosta07@gmail.com>
2022-12-21 17:59:20 -0300

    Tentando fazer o Post na API

---
+++ js/cadastro_prestadores.js
@@ -0,0 +1,47 @@
+
+let photo = document.getElementById('imgPhoto')
+// let file = document.getElementById('flImage')
+let imgPrestador = document.querySelector('#flImage')
+photo.addEventListener('click', () => {
+  imgPrestador.click()
+})
+
+imgPrestador.addEventListener('change', (event) => {
+  let reader = new FileReader();
+  reader.onload = () => {
+    photo.src = reader.result
+  }
+  reader.readAsDataURL(imgPrestador.files[0])
+})
+
+const nome = document.querySelector('#nome')
+const email = document.querySelector('#email')
+const fone = document.querySelector('#fone')
+const btnSubmit = document.querySelector('#send')
+
+btnSubmit.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  setProviderDb()
+})
+
+async function setProviderDb() {
+
+  const bodyJson = {
+    "Provider": {
+      "nome": "Testando nome 01",
+      "email": "Testando email 01"
+    },
+    "Service": {
+      "nome": "Carpintaria"
+    }
+  }
+  const response = await fetch('http://localhost/seuJoaoApi/providers/add', {
+    method: 'POST',
+    body: JSON.stringify(bodyJson)
+  })
+  const jsonData = await response.json()
+  console.log(jsonData)
+
+  // console.log(jsonData.prestadores[2].Provider.nome)
+}
