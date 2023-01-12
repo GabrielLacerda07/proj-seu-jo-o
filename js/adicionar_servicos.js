a15d2c348f586a3598c193766c71f64c8e289a08
Gabriel Lacerda <gabriellacerdacosta07@gmail.com>
2023-01-12 14:47:26 -0300

    Tentando fazer o post com o nome do servico

--- js/adicionar_servicos.js
+++ js/adicionar_servicos.js
@@ -7,14 +7,17 @@ btnSubmit.addEventListener('click', (event) => {
 })
 
 async function setServicoDb(nomeServico) {
+  const bodyJson = {
+    "Service": {
+      "nome": nomeServico
+    }
+  }
   const response = await fetch('http://localhost/seuJoaoApi/services/add', {
     method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
     body: {
-      nome: JSON.stringify(nomeServico)
+      nome: JSON.stringify(bodyJson)
     }
   })
+  const jsonData = await response.json()
   console.log(response)
 }
